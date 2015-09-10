bb9da00cffefdc64b14de5b9772f49b9b4a1fa2e
quqinglei <quqinglei@icloud.com>
2015-09-10 22:15:47 +0800

    update
    单独最后一个版本，稳定版

--- main.cpp
+++ main.cpp
@@ -9,12 +9,5 @@ int main(int argc, char *argv[])
     Connect w;
     w.show();
     w.setWindowTitle("龙泉寺皈依录入软件");
-    /*
-    QFont font = a.font();
-    font.setPointSize(16);
-    font.setBold(true);
-    font.setFamily("LiSu");
-    a.setFont(font);
-    */
     return a.exec();
 }
